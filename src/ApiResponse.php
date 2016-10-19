ac3246358d66a09ec37b0f6b6f11d0f06355d8eb
Theo Gibbons <tgib024@aucklanduni.ac.nz>
2016-10-20 09:01:43 +1300

    Added some magic methods to help with debugging

--- src/ApiResponse.php
+++ src/ApiResponse.php
@@ -4,7 +4,7 @@ namespace UberAccountingApi;
 
 use Curl\Curl;
 
-class ApiResponse
+class ApiResponse implements \JsonSerializable
 {
 
     private $curl;
@@ -239,6 +239,22 @@ class ApiResponse
 
     public function dd() {
         dj($this->serialise());
+    }
+
+    public function __debugInfo() {
+        return $this->serialise();
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     * @link http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    function jsonSerialize()
+    {
+        return $this->serialise();
     }
 
 }
